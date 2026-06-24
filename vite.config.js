298f791e5e20ff34aa5e19c41ba31af6a431f27d
idil-abdi <edilabdiaziz@gmail.com>
2026-06-24 10:49:45 +0100

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/film-filter/',
   plugins: [react(), tailwindcss(), flowbiteReact()],
 })
